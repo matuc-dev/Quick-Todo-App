9f0565d9576a001722ef934b02bd332a590f5f76
dsahddhwuihdja <sublucas.ja@gmail.com>
2024-02-11 13:10:43 +0900

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,10 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 const font = Poppins({ weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "Todo App",
-//   description: "",
-// };
+export const metadata: Metadata = {
+  title: "Todo App",
+  description: "Created By @mc_dev1",
+};
 
 export default function RootLayout({
   children,
@@ -17,12 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta property="og:title" content="Your Site Title" />
-        <meta property="og:description" content="Your site description." />
-        <meta property="og:image" content="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg" />
-      </head>
-
       <body className={font.className}>
         <ThemeProvider
           attribute="class"
