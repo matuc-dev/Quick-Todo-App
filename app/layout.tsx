5a9a774b51e1d82148e1393bea431a93924e304f
dsahddhwuihdja <sublucas.ja@gmail.com>
2024-02-11 12:43:00 +0900

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const font = Poppins({  weight: ["100", "200" ,"300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
+const font = Poppins({ weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
 
 // export const metadata: Metadata = {
 //   title: "Todo App",
@@ -17,18 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta property="og:title" content="Your Site Title" />
-          <meta property="og:description" content="Your site description." />
-          <meta property="og:image" content="https://example.com/path/to/image.jpg" />
+      <head>
+        <meta property="og:title" content="Your Site Title" />
+        <meta property="og:description" content="Your site description." />
+        <meta property="og:image" content="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg" />
+      </head>
+
       <body className={font.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-        {children}
-      </ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
