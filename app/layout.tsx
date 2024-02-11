66f764c4b20ea4fe01a7b3f563b8182307260fc7
dsahddhwuihdja <sublucas.ja@gmail.com>
2024-02-11 13:40:06 +0900

    aa

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,38 +5,14 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 const font = Poppins({ weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
 
-const info = {
-  name: "Arunava Ghosh",
-  twitter: "@arnvgh",
-  description: "19yo Software Engineer, India",
-  url: "https://mctodo.vercel.app",
-  image: "/meta/meta.png",
+export const metadata: Metadata = {
+  title: "Todo App",
+  description: "Created By @mc_dev1",
+  openGraph: {
+    images: '/opengraph-image.png',
+  },
 };
 
-export const metadata: Metadata = {
-  metadataBase: new URL(info.url),
-  title: info.name,
-  description: info.description,
-  authors: {
-    name: info.name,
-    url: info.url,
-  },
-  creator: info.name,
-  openGraph: {
-    type: "website",
-    url: info.url,
-    title: info.name,
-    description: info.description,
-    images: info.image,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: info.name,
-    description: info.description,
-    creator: info.twitter,
-    images: info.image,
-  },
-};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -44,7 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
       <body className={font.className}>
         <ThemeProvider
           attribute="class"
